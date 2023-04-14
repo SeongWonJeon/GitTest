1bacef5d94ba9a193fdde403818c9b2f27f8ecc9
SeongWon Jeon <gateoper1105@gmail.com>
2023-04-14 19:04:48 +0900

    새로운 기능2

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("굉장한기능 1개발");
-            
+            Console.WriteLine("새로운 기능1");
+            Console.WriteLine("새로운 기능2");
         }
     }
 }
